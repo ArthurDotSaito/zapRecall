bd1250054dcc650ab06542e82c8c747e78400ad0
Arthur Saito <arthur_saito1998@hotmail.com>
2022-12-02 18:20:08 -0300

    Start to create Card component

---
+++ src/components/Cards.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Cards = () =>{
+    return(
+        <CardsMainContainer>
+
+        </CardsMainContainer>
+    )
+}
+
+const CardsMainContainer = styled.main`
+`
+
+export default Cards;
